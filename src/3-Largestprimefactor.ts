f0048056b497add16d31023fee25eb457f93e98e
Chris Battarbee <chrisbattarbee3@gmail.com>
2018-07-22 14:22:21 +0100

    Complete 3

--- src/3-Largestprimefactor.ts
+++ src/3-Largestprimefactor.ts
@@ -2,23 +2,20 @@ import * as R from 'ramda';
 
 let num = 600851475143;
 
-let max = R.reduce((x : number, y : number) => x > y ? x : y);
+function factors(num : number) : Array<number> {
+    let ret = [];
+    for (let x = 1; x <= Math.sqrt(num); x ++) {
+        if (num % x == 0) {
+            ret = ret.concat([x, num / x]);
+        }
+    }
+    return ret;
+}
 
-let primeCandidates = (x : number) =>
-    R.range(1, Math.sqrt(x))
-    .map(x => [x, num % x == 0])
-    .filter(x => x[1])
-    .map(x => x[0]);
+const isPrime = (x : number) => factors(x).length == 2;
 
-console.log(primeCandidates(num));
+const largest = R.reduce((x: number, y : number) => x > y ? x : y, -Infinity);
 
-let prime = R.compose(
-    max
-    ,R.compose(
-    R.map(x => x[0])
-    ,R.compose(
-    R.filter(x => x[1] == 1),
-    R.map((x : number) => [x, primeCandidates(x)])
-)));
+const solved = R.compose(largest, R.compose(R.filter(isPrime), factors));
 
-console.log(prime(primeCandidates((num))));
+console.log(R.applyTo(num, solved));
